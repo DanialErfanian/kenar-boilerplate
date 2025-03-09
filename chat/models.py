59fb87d54f1d27ebf7b454ce42007993b7e53e39
Danial Erfanian <danial.erfanian@divar.ir>
2025-03-09 14:10:53 +0330

    Add ChatBot model

--- chat/models.py
+++ chat/models.py
@@ -15,3 +15,24 @@ class Chat(models.Model):
 
     class Meta:
         unique_together = ("post", "user_id", "peer_id")
+
+
+class ChatBot(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ChatMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    chat_bot = models.ForeignKey(ChatBot, on_delete=models.CASCADE, related_name="bot_messages", null=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages", null=True)
+
+    sender_side = models.CharField(max_length=255)
+    sender_type = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
